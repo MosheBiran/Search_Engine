c1bfe2992b6631217bc152ae7c4c1d70d80f86dd
DanielDinshtein <danieldinsh@gmail.com>
2021-01-08 15:43:22 +0200

    Searcher with func second

--- Search_Engine-master/search_engine_1.py
+++ Search_Engine-master/search_engine_1.py
@@ -47,7 +47,7 @@ class SearchEngine:
 
         indexer_dic = utils.load_obj("idx_bench")
 
-        localMethod = False
+        localMethod = True
         globalMethod = False
         wordNet = False
 
